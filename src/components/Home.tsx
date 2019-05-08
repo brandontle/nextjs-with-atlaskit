fff06e406a7edbc575465dfe47a3f6423187d210
ble <ble.message@gmail.com>
2019-05-08 19:58:46 -0400

    added @atlaskit/editor-core

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -1,15 +1,10 @@
-import * as React from 'react'
-import * as classnames from 'classnames'
-import * as css from './Home.css'
+import * as React from 'react';
+import * as classnames from 'classnames';
+import * as css from './Home.css';
+// import { Editor } from '@atlaskit/editor-core';
 
-export const Home: React.FunctionComponent = props =>
+export const Home: React.FunctionComponent = props => (
   <div className={classnames('test', css.home)}>
-    <ul>
-      <li>
-        usage classnames in Home.tsx
-      </li>
-      <li>
-        Layout.tsx set background-color hot-pink using global styled jsx
-      </li>
-    </ul>
+    {/* <Editor /> */}
   </div>
+);
